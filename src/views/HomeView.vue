d4f7a39e2ab37e2a82418b21dd1f9f89132cb90d
Vysakh-T <vysakh1801@gmail.com>
2023-06-04 21:41:57 +0530

    init

---
+++ src/views/HomeView.vue
@@ -0,0 +1,21 @@
+<!-- <script setup>
+</script> -->
+
+<template>
+  <main>
+    <p>
+      Hello World!
+    </p>
+  </main>
+</template>
+
+<style lang="scss" scoped>
+main {
+  p {
+    background-color: antiquewhite;
+    border-radius: 5px;
+    padding: 20px;
+    color: black;
+  }
+}
+</style>
