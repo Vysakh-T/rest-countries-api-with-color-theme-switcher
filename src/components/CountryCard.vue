70e6c647618f1ac1f50340e30753949716821541
Vysakh-T <vysakh1801@gmail.com>
2023-06-18 00:21:00 +0530

    add store and country card styling

---
+++ src/components/CountryCard.vue
@@ -0,0 +1,59 @@
+<script setup>
+// Add validation
+const props = defineProps({
+    name: String,
+    population: Number,
+    region: String,
+    capital: String,
+    flagLink: String,
+})
+const numLocale = 'en-US'
+</script>
+
+<template>
+    <div class="card">
+        <img :src="flagLink" :alt="'Flag of ' + props.name" />
+        <div class="details">
+            <h4>
+                {{ props.name }}
+            </h4>
+            <p><b>Population:</b> {{ props.population.toLocaleString(numLocale) }}</p>
+            <p><b>Region:</b> {{ props.region }}</p>
+            <p><b>Capital:</b> {{ props.capital }}</p>
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+.card {
+    background: white;
+    width: 100%;
+    box-shadow: 0 0px 10px 0px rgba(0, 0, 0, 0.1);
+    border-radius: 5px;
+    overflow: hidden;
+
+    img {
+        width: 100%;
+        object-fit: cover;
+        height: 23vh;
+    }
+
+    .details {
+        padding-block: 1vw 2.5vw;
+        padding-inline: 2vw;
+        width: 100%;
+
+        h4 {
+            padding-bottom: 0.5vw;
+            font-weight: 800;
+            font-size: 16px;
+        }
+
+        p {
+            b {
+                font-weight: 600;
+            }
+        }
+    }
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,32 @@
-<script setup> 
-  import HeaderVue from '../components/Header.vue';
+<script setup>
+import HeaderVue from '../components/Header.vue';
+import CountryCardVue from '../components/CountryCard.vue';
+import { useCountriesStore } from '@/stores/countries';
+
+const countriesStore = useCountriesStore()
+const { data } = countriesStore
 </script>
 
 <template>
-  <HeaderVue/>
+  <HeaderVue />
+  <main>
+    <section>
+      <CountryCardVue :name="i.name" :population="i.population" :region="i.region" :capital="i.capital" :flag-link="i?.flags.svg || i?.flag"
+        v-for="i in data" />
+    </section>
+  </main>
 </template>
 
 <style lang="scss" scoped>
+main {
+  padding: 4vh 5vw;
+
+  section {
+    display: grid;
+    grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
+    grid-gap: 4vw;
+    justify-items: center;
+    align-items: center;
+  }
+}
 </style>
