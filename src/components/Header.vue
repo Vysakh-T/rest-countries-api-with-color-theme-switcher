e355b607b7c32d1feb7bfa053f6ddc66d102c33f
Vysakh-T <vysakh1801@gmail.com>
2023-06-18 11:37:03 +0530

    small fixes

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -6,6 +6,8 @@
 
 <style lang="scss" scoped>
     header {
+        // position: fixed;
+        // width: 100%;
         background-color: var(--color-elements);
         padding: 2vh 5vw;
         box-shadow: 0 1px 5px 2px rgba(0,0,0,0.1);
--- src/components/SearchBox.vue
+++ src/components/SearchBox.vue
@@ -1,5 +1,10 @@
+<script setup>
+import SearchIcon from './icons/SearchIcon.vue';
+</script>
+
 <template>
     <div>
+        <SearchIcon />
         <p>Search for a country...</p>
     </div>
 </template>
@@ -11,5 +16,14 @@
         padding: 1vw 1.5vw;
         box-shadow: 0 0px 10px 0px rgba(0, 0, 0, 0.1);
         border-radius: 3px;
+        display: flex;
+        justify-content: start;
+        align-items: center;
+        p{
+            margin-left: 1vw;
+        }
+        svg {
+            fill: var(--color-input);
+        }
     }
 </style>
