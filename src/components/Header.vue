4148072481cfa2a3a53f1601d5e4d21845436ec8
Vysakh T <vysakh1801@gmail.com>
2024-10-14 12:08:22 +0530

    format

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -5,15 +5,16 @@
 </template>
 
 <style lang="scss" scoped>
-    header {
-        // position: fixed;
-        // width: 100%;
-        background-color: var(--color-elements);
-        padding: 2vh 5vw;
-        box-shadow: 0 1px 5px 2px rgba(0,0,0,0.1);
-        h1 {
-            font-weight: 800;
-            font-size: 20px;
-        }
+header {
+    // position: fixed;
+    // width: 100%;
+    background-color: var(--color-elements);
+    padding: 2vh 5vw;
+    box-shadow: 0 1px 5px 2px rgba(0, 0, 0, 0.1);
+
+    h1 {
+        font-weight: 800;
+        font-size: 20px;
     }
+}
 </style>
